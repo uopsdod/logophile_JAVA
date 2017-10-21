b23f08ef8d4d489ef53b6d7dc45331b2d275acaf
SamTsai <uopsdod@gmail.com>
2017-10-21 21:01:50 +0800

    #TASK - 1. create MyImplicitStrategy & MyPhysicalStartegy 2. replace ImplicitNamingStrategyLegacyJpaImpl with MyImplicitStrategy

---
+++ Logophile_JAVA/src/main/java/com/model/MyImplicitStrategy.java
@@ -0,0 +1,18 @@
+package com.model;
+
+import org.hibernate.boot.model.naming.Identifier;
+import org.hibernate.boot.model.naming.ImplicitJoinColumnNameSource;
+import org.hibernate.boot.model.naming.ImplicitNamingStrategyLegacyJpaImpl;
+
+public class MyImplicitStrategy extends ImplicitNamingStrategyLegacyJpaImpl {
+
+	// ref: https://github.com/hibernate/hibernate-orm/blob/master/hibernate-core/src/main/java/org/hibernate/boot/model/naming/ImplicitNamingStrategyLegacyJpaImpl.java
+	@Override
+	public Identifier determineJoinColumnName(ImplicitJoinColumnNameSource source) {
+		String name = source.getReferencedColumnName().getText();
+		return toIdentifier( name, source.getBuildingContext() );
+		// TODO Auto-generated method stub
+//		return super.determineJoinColumnName(arg0);
+	}
+	
+}
---
+++ Logophile_JAVA/src/main/java/com/model/MyPhysicalStartegy.java
@@ -0,0 +1,12 @@
+package com.model;
+
+import java.io.Serializable;
+import java.util.Locale;
+
+import org.hibernate.boot.model.naming.Identifier;
+import org.hibernate.boot.model.naming.PhysicalNamingStrategyStandardImpl;
+import org.hibernate.engine.jdbc.env.spi.JdbcEnvironment;
+
+public class MyPhysicalStartegy extends PhysicalNamingStrategyStandardImpl implements Serializable {
+	// ref: https://github.com/hibernate/hibernate-orm/blob/master/hibernate-core/src/main/java/org/hibernate/boot/model/naming/PhysicalNamingStrategyStandardImpl.java
+}
