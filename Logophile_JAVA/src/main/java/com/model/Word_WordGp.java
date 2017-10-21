7f834a63e07cbc0f5c4fee9c76333af8e346f096
SamTsai <uopsdod@gmail.com>
2017-10-21 20:34:06 +0800

    #TASK - create Word_WordGp.java

---
+++ Logophile_JAVA/src/main/java/com/model/Word_WordGp.java
@@ -0,0 +1,76 @@
+package com.model;
+
+import javax.persistence.*;
+
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.hibernate.annotations.GenericGenerator;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.model.common.Common;
+import com.model.pic.Pic;
+import com.model.rate.Rate;
+
+import java.util.Date;
+import java.util.List;
+
+@Entity
+public class Word_WordGp extends Common{
+
+	// Why should not use the AUTO JPA GenerationType with MySQL and Hibernate:
+    // https://vladmihalcea.com/2017/01/24/why-should-not-use-the-auto-jpa-generationtype-with-mysql-and-hibernate/
+    @Id
+    @GeneratedValue(
+        strategy= GenerationType.AUTO, 
+        generator="word_WordGpId_native"
+    )
+    @GenericGenerator(
+        name = "word_WordGpId_native", 
+        strategy = "native"
+    )
+    Long word_WordGpId;
+    
+	@JsonIgnore // to prevent infinite loop when parsing into json
+    @ManyToOne(optional = false)
+    private Word wordId; // FK
+	
+	@JsonIgnore // to prevent infinite loop when parsing into json
+    @ManyToOne(optional = false)
+	private WordGp wordGpId; // FK
+	
+    public Word_WordGp(Date createDate) {
+        this.createDate = createDate;
+    }
+
+    public Word_WordGp() {
+    }
+    
+	public Long getWord_WordGpId() {
+		return word_WordGpId;
+	}
+
+	public void setWord_WordGpId(Long word_WordGpId) {
+		this.word_WordGpId = word_WordGpId;
+	}
+
+	public Word getWordId() {
+		return wordId;
+	}
+
+	public void setWordId(Word wordId) {
+		this.wordId = wordId;
+	}
+
+	public WordGp getWordGpId() {
+		return wordGpId;
+	}
+
+	public void setWordGpId(WordGp wordGpId) {
+		this.wordGpId = wordGpId;
+	}
+
+	@Override
+	public String toString() {
+		return ToStringBuilder.reflectionToString(this);
+	}
+    
+}
